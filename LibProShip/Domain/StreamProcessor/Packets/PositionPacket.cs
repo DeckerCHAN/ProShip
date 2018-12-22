c224df4f6b9d27d61ec2a05c9480a4ff40e17467
DeckerCHAN <DeckerCHAN@gmail.com>
2018-12-22 12:19:00 +1100

    Add packets

---
+++ LibProShip/Domain/StreamProcessor/Packets/PositionPacket.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace LibProShip.Domain.StreamProcessor.Packets
+{
+    public sealed class Packets
+    {
+        public IEnumerable<PositionPacket> PositionPacket { get; private set; }
+    }
+    
+    public class PositionPacket
+    {
+        
+    }
+
+
+    public class ProjectilePacket
+    {
+        
+    }
+    
+    
+}
