b5c714b0eb51d4e779d4adc3d6bb4aa6d17abff8
Derek Chan <derek.chan@ratesetter.com.au>
2018-12-28 10:20:25 +1100

    Add PyPickle test

---
+++ LibProShip.Test/Integration/PythonPickelTest.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Razorvine.Pickle;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LibProShip.Test.Integration
+{
+    public class PythonPickelTest
+    {
+        private readonly ITestOutputHelper TestOutputHelper;
+
+        public PythonPickelTest(ITestOutputHelper testOutputHelper)
+        {
+            TestOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void Test()
+        {
+            // going to pickle a c# datastructure
+
+            var map = new Dictionary<string, object>
+            {
+                ["apple"] = 42,
+                ["microsoft"] = "hello"
+            };
+            var values = new List<double>();
+            values.AddRange(new[] {1.11, 2.22, 3.33, 4.44, 5.55});
+            map["values"] = values;
+            // You can add many other types if you like. See the readme about the type mappings.
+
+            const string pickleFilename = "testpickle.dat";
+
+            TestOutputHelper.WriteLine("Writing pickle to '{0}'", pickleFilename);
+
+            var pickler = new Pickler(true);
+            using (FileStream fos = new FileStream(pickleFilename, FileMode.Create))
+            {
+                pickler.dump(map, fos);
+            }
+
+            TestOutputHelper.WriteLine("Done. Try unpickling it in python.\n");
+
+            TestOutputHelper.WriteLine("Reading a pickle created in python...");
+
+            // the following pickle was created in Python 3.4.
+            // it is this data:     [1, 2, 3, (11, 12, 13), {'banana', 'grape', 'apple'}]
+            byte[] pythonpickle =
+            {
+                128, 4, 149, 48, 0, 0, 0, 0, 0, 0, 0, 93, 148, 40, 75, 1, 75, 2, 75, 3, 75, 11, 75, 12, 75, 13, 135,
+                148, 143, 148, 40, 140, 6, 98, 97, 110, 97, 110, 97, 148, 140, 5, 103, 114, 97, 112, 101, 148, 140, 5,
+                97, 112, 112, 108, 101, 148, 144, 101, 46
+            };
+
+
+            using (var stream = new FileStream("arena.pxl", FileMode.Open))
+            {
+                pythonpickle = new Byte [stream.Length];
+                stream.Read(pythonpickle, 0, pythonpickle.Length);
+            }
+
+            var unpickler = new Unpickler();
+            object result = unpickler.loads(pythonpickle);
+
+            TestOutputHelper.WriteLine("type: {0}", result.GetType());
+            var list = (ArrayList) result;
+            int integer1 = (int) list[0];
+            int integer2 = (int) list[1];
+            int integer3 = (int) list[2];
+            var tuple = (object[]) list[3];
+            var set = (HashSet<object>) list[4];
+            TestOutputHelper.WriteLine("1-3: integers: {0}, {1}, {2}", integer1, integer2, integer3);
+            TestOutputHelper.WriteLine("4: tuple: ({0}, {1}, {2})", tuple[0], tuple[1], tuple[2]);
+            TestOutputHelper.WriteLine("5: set: {0}", string.Join(",", set));
+
+            TestOutputHelper.WriteLine("\r\nEnter to exit:");
+        }
+    }
+}
