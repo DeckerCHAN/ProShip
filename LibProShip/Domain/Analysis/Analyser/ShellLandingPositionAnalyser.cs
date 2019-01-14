229c55670bb5c58ece3cbaedd8155ad01a0ffacf
Derek Chan <derek.chan@ratesetter.com.au>
2019-01-14 12:22:33 +1100

    Fixed missing member in ShellLandingPositionAnalyser.

--- LibProShip/Domain/Analysis/Analyser/ShellLandingPositionAnalyser.cs
+++ LibProShip/Domain/Analysis/Analyser/ShellLandingPositionAnalyser.cs
@@ -5,6 +5,13 @@ namespace LibProShip.Domain.Analysis.Analyser
 {
     public class ShellLandingPositionAnalyser : SphereAnalyserBase
     {
+        public ShellLandingPositionAnalyser()
+        {
+            Name = "Shell Landing Analysis";
+        }
+
+        public override string Name { get; }
+
         public override AnalysisCollection Analysis(BattleRecord battleRecord)
         {
             throw new System.NotImplementedException();
